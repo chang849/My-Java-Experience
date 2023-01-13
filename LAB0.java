275f3729f887ddad82998f6b49446828cff4aab5
S.Chang <114774070+chang849@users.noreply.github.com>
2023-01-13 15:50:46 -0500

    Add files via upload
    This is me scratching the surface as I begin to improve and expand upon my knowledge with Java.

---
+++ LAB0.java
@@ -0,0 +1,7 @@
+public class LAB0 {
+public static void main(String args[]) {
+        System.out.println("Hi. My name is Seth");
+        System.out.println("My major is Copmuter & Information Technology");
+        System.out.println("My favorite Programming Language is Java");
+    }
+}
